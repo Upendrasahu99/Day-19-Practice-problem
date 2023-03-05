d549b70b83615e4bc79057e1f4409290ffad8039
upendra <upendrasahu1199@gmail.com>
2023-03-05 23:20:52 +0530

    Enter first name of user

--- src/main/java/com/bridgelabz/user_regstration_problem/UserRegistration.java
+++ src/main/java/com/bridgelabz/user_regstration_problem/UserRegistration.java
@@ -1,8 +1,30 @@
 package com.bridgelabz.user_regstration_problem;
 
+import java.util.Scanner;
+
 public class UserRegistration {
+
+    String firstName;
+
+    UserRegistration(int user){
+        System.out.println("User " + user);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter user first name : ");
+        this.firstName = scanner.nextLine();
+    }
+   public boolean validationOfFirstName(){
+        if(firstName.matches("[A-Z][a-z]{2,}")){
+            return true;
+        }else{
+            System.out.println("Invalid data");
+         return false;
+        }
+    }
 
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration Program");
+
+        UserRegistration user1 = new UserRegistration(1);
+        user1.validationOfFirstName();
     }
 }
